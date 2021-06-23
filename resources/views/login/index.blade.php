6c8d6c0099b494f4524446868cb340283acdbd3d
Debashish-Dey-DM <deydebashish71@gmail.com>
2021-06-23 11:29:06 +0600

    initail commit

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<form method="post">
+	<table>
+		<!-- @csrf -->
+		<!-- {{csrf_field()}} -->
+		<input type="hidden" name="_token" value="{{csrf_token()}}">
+		<tr>
+			<td>Email</td>
+			<td><input type="text" name="email" ></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="Submit" value="submit"></td>
+		</tr>
+	</table>
+	</form>
+		{{-- {{session('msg')}}
+		<br>
+
+		{{-- @foreach ($errors->all() as $err)
+			{{$err}} <br>
+		@endforeach --}}
+</body> 
+</html>
